19380a637f5fe82dc9457760e054676154da5ce6
villain <jmbengal.mbstu@gmail.com>
2022-08-15 21:51:37 +0600

    Selection sort added

---
+++ Sorting/Selection sort.py
@@ -0,0 +1,14 @@
+def selectionSort(data):
+    l=len(data)
+    for i in range(l):
+        min_index=i
+        for j in range(i+1,l):
+            if data[i]>data[j]:
+                min_index=j
+        data[i], data[min_index]=data[min_index], data[i]
+    return data
+
+data=list(map(int, input('Enter the numbers that will be sorted\n').strip().split()))
+print('The arrays are before sorting => ',data)
+data=selectionSort(data)
+print('The arrays are after sorting  => ',data)
