9ca76266c74023e94348b82d90000e30e11ff987
villain <jmbengal.mbstu@gmail.com>
2022-08-20 21:41:47 +0600

    Cycle in graph using DFS

--- Graph/Cycle in a graph-DFS.py
+++ Graph/Cycle in a graph-DFS.py
@@ -1,36 +1,25 @@
-# Source
-#
-# & customized
+# Title: Cycle detection using DFS
+# Source: own
+# Theory: https://www.youtube.com/watch?v=Pg9ZXkQgRaE
+
+# Theory: If visited node is not parent node, then Cycle is existed
 
 from collections import defaultdict
-
-
-def dfs(visited, graph, root, parent_node=-1):  # function for dfs
-    visited.add(root)
-    for neighbour in graph[root]:
-        if neighbour not in visited:
-            if dfs(visited, graph, neighbour, root) in visited:
+def dfs(graph, visited, root, parent_node=-1):  # function for dfs
+    # print('Parent+> ', parent_node)
+    visited.add(root) # make root nod visited
+    for neighbour in graph[root]: # adjacent node
+        if neighbour not in visited: # if adj not visited, run dfs
+            if dfs(graph, visited, neighbour, root): # if adj return true, cycle found
                 print('dfs-1')
-                return 1
-            elif neighbour != root:
+                return True
+        else:
+            if neighbour != parent_node: # if visited and adj & parent node are same, cycle found
                 print('Not root-1')
-                return 1
-        # dfs(visited, graph, neighbour)
-    print('Terminated')
-    return 0
+                return True
+    # print('Terminated')
+    return False # if above condition false, Cycle not found
 
-
-# making graph according to user input.
-graph = defaultdict(list)
-n = int(input('Enter the number of connecting roads => '))
-for i in range(n):
-    x, y = map(int, input().split())
-    if y not in graph[x]:  # not repeat valu
-        graph[x].append(y)
-    if x not in graph[y]:  # not repeat valu
-        graph[y].append(x)
-root = int(input('Enter the root node: '))
-# print('Graph is ==> ', graph)
 
 '''
 7
@@ -43,13 +32,25 @@ root = int(input('Enter the root node: '))
 4 5    cycle
 '''
 
+# making graph according to user input.
+graph = defaultdict(list)
+n = int(input('Enter the number of connecting roads => '))
+for i in range(n):
+    x, y = map(int, input().split())
+    if y not in graph[x]:  # not repeat value
+        graph[x].append(y)
+    if x not in graph[y]:  # not repeat value
+        graph[y].append(x)
+root = int(input('Enter the root node: '))
+print('Graph is ==> ', graph)
+
+
 visited = set()  # used as stack
 # Driver Code
-# print("Following is the Depth-First Search")
 cycle = 0
-result = dfs(visited, graph, root)
+result = dfs(graph, visited, root)
 print('Result==> ', result)
-if result == 1:
+if result:
     print('Cycle found')
 else:
     print('Cycle not found')
