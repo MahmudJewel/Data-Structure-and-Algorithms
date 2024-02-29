5dc70c1e6638fdeb080f1db9f33ec8ea9da53cd6
Mahmud Jewel <jmbengal.mbstu@gmail.com>
2024-02-28 23:36:23 -0500

    Added fibonacci optimized recursion

--- Recursion/Fibonacci.py
+++ Recursion/Fibonacci.py
@@ -1,12 +1,57 @@
-# This program runs until n=38
-def making_fibonacci_series(n):
-    if n<=1:
-        return  n
-    return (making_fibonacci_series(n-1)+making_fibonacci_series(n-2))
+# # This program runs until n=38
+# def making_fibonacci_series(n):
+#     if n<=1:
+#         return  n
+#     return (making_fibonacci_series(n-1)+making_fibonacci_series(n-2))
 
-n=int(input('Enter a number '))
-fibonacci_list=[]
-for i in range(n):
-    fibonacci_list.append(making_fibonacci_series(i))
-print(*fibonacci_list)
+# n=int(input('Enter a number '))
+# fibonacci_list=[]
+# for i in range(n):
+#     fibonacci_list.append(making_fibonacci_series(i))
+# print(*fibonacci_list)
 
+# Function for nth Fibonacci number
+
+# def Fibonacci(n):
+#     if n<= 0:
+#         print("Incorrect input")
+#     # First Fibonacci number is 0
+#     elif n == 1:
+#         return 0
+#     # Second Fibonacci number is 1
+#     elif n == 2:
+#         return 1
+#     else:
+#         return Fibonacci(n-1)+Fibonacci(n-2)
+
+# # Driver Program
+
+# print(Fibonacci(40))
+
+# =================================================================
+"""
+This is faster and written solely by me.
+Nth fibonacci number 
+"""
+
+def get_fibonacci(iterator: int, n: int, a: int, b: int):
+    # validity check
+    if n < 0:
+        return "Please enter a positive number"
+    elif n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        c = a + b
+        a, b = b, c
+        iterator += 1
+        if iterator == n:
+            return c
+        else:
+            return get_fibonacci(iterator, n, a, b)
+
+
+n = int(input("Enter a number "))
+result = get_fibonacci(2, n, 0, 1)
+print("result", result)
--- Recursion/fibonacci_normal.py
+++ Recursion/fibonacci_normal.py
@@ -1,10 +1,14 @@
 n=int(input('Enter a number: '))
 fibonacci_list=[]
-n1,n2=0,1
-fibonacci_list.append(n1)
-fibonacci_list.append(n2)
-for i in range(2,n):
-    n3=n1+n2
-    fibonacci_list.append(n3)
-    n1,n2=n2,n3
-print(*fibonacci_list)
+# validity check 
+if n<1:
+    print ('Please enter a positive number')
+else:
+    n1,n2=0,1
+    fibonacci_list.append(n1)
+    fibonacci_list.append(n2)
+    for i in range(2,n):
+        n3=n1+n2
+        fibonacci_list.append(n3)
+        n1,n2=n2,n3
+    print(*fibonacci_list)
